efc4b3f4cb5997823eb0246f187a48fc1802f47d
zzk <15688506062@163.com>
2026-03-30 16:54:54 +0900

    commint

---
+++ lib/1.dart
@@ -0,0 +1,13 @@
+void main(){
+  String lastName="ZHAO";
+  String firstName="yu";
+  print("name is $lastName $firstName");
+  int num1 = 10;
+  double pi = 3.14;
+  double num3 = pi * num1;
+  double num3 = pi * num1;
+  num num4  = pi * num1;
+  print("num3:$num3  num4:$num4");
+
+
+}
